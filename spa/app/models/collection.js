2bf80e1f78fe23234fa5fe7b2a85d77509b697f4
Emmanuel Barachant <e.barachant@samsung.com>
2015-06-16 18:15:21 +0200

    Test for local runtime on Apache

--- spa/app/models/collection.js
+++ spa/app/models/collection.js
@@ -8,6 +8,7 @@
 // Backbone de apps-list).  On exploite ici plusieurs aspects
 // pratiques des collections Backbone.
 
+
 module.exports = Backbone.Collection.extend({
   // Définition du modèle à exploiter lors d'ajouts, fetches, etc.
   // Du coup, on peut passer juste des hashes d'attributs, ça
