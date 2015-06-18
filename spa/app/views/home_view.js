002146044165eccb212bef9886723a3705a59130
Emmanuel Barachant <emmbar.dev+gituser@gmail.com>
2015-06-18 23:39:44 +0200

    Add some role

--- spa/app/views/home_view.js
+++ spa/app/views/home_view.js
@@ -32,7 +32,8 @@ module.exports = View.extend({
   // peuple le template principal avec ces données.
   getRenderData: function() {
     return {
-      alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+      alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+      otherdata: 'fake'
     };
   },
 
