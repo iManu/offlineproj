3cd170428d0812bb0d9c563910cf3c9091ee443f
Emmanuel Barachant <e.barachant@samsung.com>
2015-07-06 17:58:58 +0200

    Shut down reload prompt to an automatic reload behavior

--- spa/app/lib/appcache.js
+++ spa/app/lib/appcache.js
@@ -6,9 +6,15 @@
 if('applicationCache' in window) {
   window.applicationCache.addEventListener('updateready', function () {
     //alert('Rafraichir S.V.P.');
-    $('#reloadPrompt').addClass('show');
+    //$('#reloadPrompt').addClass('show');
 
-  });
+    // No prompt, on recharge methode meme pas mal
+    if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+      window.applicationCache.swapCache();
+      window.location.reload();
+    }
+
+  }, false);
   // si la page n'est jamais rechargé, ce qui peut arriver en SPA
   setInterval(applicationCache.update, 100000000000000);
 }
