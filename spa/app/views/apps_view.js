b4641a8222c74c90ce11e7b291390ba711f50dbc
Emmanuel Barachant <e.barachant@samsung.com>
2015-07-07 09:31:57 +0200

    Set styles rwd for carousel

--- spa/app/views/apps_view.js
+++ spa/app/views/apps_view.js
@@ -231,28 +231,36 @@ module.exports = View.extend({
         that.contentItems.find('.app-carousel').slick({
           dots: false,
           arrows: true,
-          infinite: true,
+          infinite: false,
           adaptiveHeight: true,
-          centerMode: true,
+          centerMode: false,
           variableWidth: true,
           speed: 300,
           slidesToShow: 3,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           responsive: [
             {
               breakpoint: 1024,
               settings: {
                 slidesToShow: 3,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true
+                slidesToScroll: 1,
+                infinite: false,
+                dots: false,
+                adaptiveHeight: true,
+                centerMode: false,
+                variableWidth: true
               }
             },
             {
               breakpoint: 480,
               settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                infinite: false,
+                dots: false,
+                adaptiveHeight: true,
+                centerMode: false,
+                variableWidth: true
               }
             }
             // You can unslick at a given breakpoint now by adding:
